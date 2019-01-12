db0e07ec51f48c94e55f6c4867d0cb74d04555d3
maciektr <maciek2tr99@gmail.com>
2019-01-12 20:05:26 +0100

    Add number of residents module

---
+++ numberResidents.py
@@ -0,0 +1,4 @@
+
+def numberResidents():
+    res = {"Mazowieckie":5349114,"Slaskie":4570849,"Wielkopolskie":3475323,"Malopolskie":3372618,"Dolnoslaskie":2904207,"Lodzkie":2493603,"Pomorskie":2307710,"Lubelskie":2139726,"Podkarpackie":2127657,"Kujawsko-pomorskie":2086210,"Zachodnio-pomorskie":1710482,"Warminsko-mazurskie":1439675,"Swietokrzyskie":1257179,"Podlaskie":1188800,"Lubuskie":1018075,"Opolskie":996011}
+    return res
