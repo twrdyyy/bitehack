2871c1d568c48ce9801e2f87963be394e1ee4228
maciektr <maciek2tr99@gmail.com>
2019-01-12 18:48:28 +0100

    Single Health data parser Global url dictionary module

--- getDataUrl.py
+++ getDataUrl.py
@@ -1,26 +1,28 @@
 from apiclient import APIClient
+import healthDictionary as tumor
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 class daneGovPl(APIClient):
     BASE_URL = 'https://api.dane.gov.pl/resources/'
     def callDataset(self,ID):
-        # print(self.call(ID))
         return self.call(ID)['data']['attributes']['file_url']
 
-def getUrlList():
-    res = []
-    daneGov = daneGovPl()
-     
-    res.append(tuple(["health","tumor",daneGov.callDataset('3945')]))
-    res.append(tuple(["safety","murder",daneGov.callDataset('10420')]))
-    res.append(tuple(["safety","rape",daneGov.callDataset('10445')]))
-    res.append(tuple(["safety","theft",daneGov.callDataset('10431')]))
-    res.append(tuple(["safety","drugs",daneGov.callDataset('10451')]))
-    res.append(tuple(["safety","battery",daneGov.callDataset('5938')]))
-
-
+def getUrlDict():
+    res = {"health":{},"safety":{}}
+    daneGov = daneGovPl()     
+    res["health"].update({"tumor":daneGov.callDataset('3945')})
+    res["safety"].update({"murder":daneGov.callDataset('10420')})
+    res["safety"].update({"rape":daneGov.callDataset('10445')})
+    res["safety"].update({"theft":daneGov.callDataset('10431')})
+    res["safety"].update({"drugs":daneGov.callDataset('10451')})
+    res["safety"].update({"battery":daneGov.callDataset('5938')})
+    res["health"].update({"health":'https://stat.gov.pl/download/gfx/portalinformacyjny/pl/defaultaktualnosci/5513/6/6/1/stan_zdrowia_ludnosci_polski_w_2014_r-tablie.zip'})
+    res["economy"].update({"unemployment":"https://stat.gov.pl/download/gfx/portalinformacyjny/pl/defaultaktualnosci/5473/2/39/1/pow11_15____.xls"})
     return res
 
 if __name__ == '__main__':
-    print(getUrlList())   
+    # tumor.tumorDictionary(getUrlDict()['health']['tumor'])
+    # print(tumor.healthDictionary(getUrlDict()['health']['health']))
+    # print(getUrlDict())
+    #print(tumor.healthDictionary(getUrlDict()['health']))
--- healthDictionary.py
+++ healthDictionary.py
@@ -5,12 +5,21 @@ import zipfile
 import io
 import math
 
-posSearch = ['Samoocena stanu zdrowia', 'Długotrwałe problemy zdrowotne', 'Chorujący przewlekle', 'Niepełnosprawność wg kryterium polskiego', 'Opóźnienia w dostępie z powodu długiego  czasu oczekiwania']
+categoriesSearch = ['Samoocena stanu zdrowia', 'Długotrwałe problemy zdrowotne', 'Chorujący przewlekle', 'Niepełnosprawność wg kryterium polskiego', 'Opóźnienia w dostępie z powodu długiego  czasu oczekiwania','Nowotwory']
 
 def healthCriteria():
     return posSearch
 
-def healthDictionary(arg1):
+def healthDictionary(healthUrls):
+    mainDict = mainHealthDictionary(healthUrls['health'])
+    tumorDict = tumorDictionary(healthUrls['tumor'])
+    for wojewodztwo in mainDict.keys():
+        mainDict[wojewodztwo]={**mainDict[wojewodztwo],**tumorDict[wojewodztwo]}
+    return mainDict
+
+
+def mainHealthDictionary(arg1):
+    posSearch = ['Samoocena stanu zdrowia', 'Długotrwałe problemy zdrowotne', 'Chorujący przewlekle', 'Niepełnosprawność wg kryterium polskiego', 'Opóźnienia w dostępie z powodu długiego  czasu oczekiwania']
     wojTable = ['Dolnoslaskie', 'Kujawsko-pomorskie', 'Lubelskie', 'Lubuskie', 'Lodzkie', 'Malopolskie', 'Mazowieckie', 'Opolskie', 'Podkarpackie', 'Podlaskie', 'Pomorskie', 'Slaskie', 'Swietokrzyskie', 'Warminsko-mazurskie', 'Wielkopolskie', 'Zachodnio-pomorskie' ]
     retDictionary = {wojTable[0]:{}, wojTable[1]:{}, wojTable[2]:{}, wojTable[3]:{}, wojTable[4]:{}, wojTable[5]:{}, wojTable[6]:{}, wojTable[7]:{}, wojTable[8]:{}, wojTable[9]:{}, wojTable[10]:{}, wojTable[11]:{}, wojTable[12]:{}, wojTable[13]:{}, wojTable[14]:{}, wojTable[15]:{}, "normal":{}}
     r = requests.get(arg1, stream=True)
@@ -22,19 +31,38 @@ def healthDictionary(arg1):
             for i in tablica1:
                 for j in posSearch:
                     if i == j:
-                        tmpCount=4;
+                        tmpCount=4
                         maks=0.0
                         for k in wojTable:
                             woj=wojTable[tmpCount-4]
-                            tmpName='Unnamed: ' + str(tmpCount);
-                            tmpCount=tmpCount+1;
+                            tmpName='Unnamed: ' + str(tmpCount)
+                            tmpCount=tmpCount+1
                             retDictionary[woj][i]=baza[tmpName][itCount-1]
                             if baza[tmpName][itCount-1] > maks:
                                 maks = baza[tmpName][itCount-1]
                         retDictionary["normal"][i]=maks
                 itCount=itCount+1
+    
     return retDictionary
 
+def tumorDictionary(url):
+    wojTable = ['Dolnoslaskie', 'Kujawsko-pomorskie', 'Lubelskie', 'Lubuskie', 'Lodzkie', 'Malopolskie', 'Mazowieckie', 'Opolskie', 'Podkarpackie', 'Podlaskie', 'Pomorskie', 'Slaskie', 'Swietokrzyskie', 'Warminsko-mazurskie', 'Wielkopolskie', 'Zachodnio-pomorskie' ]
+    retDictionary = {wojTable[0]:{}, wojTable[1]:{}, wojTable[2]:{}, wojTable[3]:{}, wojTable[4]:{}, wojTable[5]:{}, wojTable[6]:{}, wojTable[7]:{}, wojTable[8]:{}, wojTable[9]:{}, wojTable[10]:{}, wojTable[11]:{}, wojTable[12]:{}, wojTable[13]:{}, wojTable[14]:{}, wojTable[15]:{}}
+
+    data = pd.read_csv(url, delimiter=';');    
+    data = data.drop('ICD10',axis=1)
+    data = data.drop('Plec',axis=1)
+    data = data.drop('Grupa_wieku',axis=1)
+    year = data['Rok'].max()
+    data = data[data['Rok']==year]
+    data = data.drop('Rok', axis=1)
+    grouped = data.groupby('Wojewodztwo').sum().reset_index()
+    normal = grouped['Liczba'].max()
+    retDictionary['normal']={'Nowotwory':normal}
+    for index,row in grouped.iterrows():
+        retDictionary[wojTable[row['Wojewodztwo']//2-1]]={"Nowotwory":row['Liczba']}
+    # print(retDictionary)
+    return retDictionary
 
 '''
 #Kod do testowania, czy zwraca poprawne wartości
