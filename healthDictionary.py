3b598f22950adbf3a218861cb8f2933d33b23948
Daniel Barczyk <46358936+DanielBarczyk@users.noreply.github.com>
2019-01-12 17:47:45 +0100

    Dodanie wartości normalizującej

--- healthDictionary.py
+++ healthDictionary.py
@@ -5,12 +5,10 @@ import zipfile
 import io
 import math
 
-
 def healthDictionary(arg1):
     posSearch = ['Samoocena stanu zdrowia', 'Długotrwałe problemy zdrowotne', 'Chorujący przewlekle', 'Niepełnosprawność wg kryterium polskiego', 'Opóźnienia w dostępie z powodu długiego  czasu oczekiwania']
     wojTable = ['Dolnoslaskie', 'Kujawsko-pomorskie', 'Lubelskie', 'Lubuskie', 'Lodzkie', 'Malopolskie', 'Mazowieckie', 'Opolskie', 'Podkarpackie', 'Podlaskie', 'Pomorskie', 'Slaskie', 'Swietokrzyskie', 'Warminsko-mazurskie', 'Wielkopolskie', 'Zachodnio-pomorskie' ]
-    retDictionary = {wojTable[0]:{}, wojTable[1]:{}, wojTable[2]:{}, wojTable[3]:{}, wojTable[4]:{}, wojTable[5]:{}, wojTable[6]:{}, wojTable[7]:{}, wojTable[8]:{}, wojTable[9]:{}, wojTable[10]:{}, wojTable[11]:{}, wojTable[12]:{}, wojTable[13]:{}, wojTable[14]:{}, wojTable[15]:{}}
-
+    retDictionary = {wojTable[0]:{}, wojTable[1]:{}, wojTable[2]:{}, wojTable[3]:{}, wojTable[4]:{}, wojTable[5]:{}, wojTable[6]:{}, wojTable[7]:{}, wojTable[8]:{}, wojTable[9]:{}, wojTable[10]:{}, wojTable[11]:{}, wojTable[12]:{}, wojTable[13]:{}, wojTable[14]:{}, wojTable[15]:{}, "normal":{}}
     r = requests.get(arg1, stream=True)
     with zipfile.ZipFile(io.BytesIO(r.content)) as z:
         with z.open("P1.xlsx") as f:
@@ -21,11 +19,15 @@ def healthDictionary(arg1):
                 for j in posSearch:
                     if i == j:
                         tmpCount=4;
+                        maks=0.0
                         for k in wojTable:
                             woj=wojTable[tmpCount-4]
                             tmpName='Unnamed: ' + str(tmpCount);
                             tmpCount=tmpCount+1;
                             retDictionary[woj][i]=baza[tmpName][itCount-1]
+                            if baza[tmpName][itCount-1] > maks:
+                                maks = baza[tmpName][itCount-1]
+                        retDictionary["normal"][i]=maks
                 itCount=itCount+1
     return retDictionary
 
