ca5a89aafa704c4e84f34ec982f0829a702dc42a
woocash33 <39830781+woocash33@users.noreply.github.com>
2019-01-12 20:58:23 +0100

    Update safetyDictionary.py
    Safety Dictionary

--- safetyDictionary.py
+++ safetyDictionary.py
@@ -1,10 +1,10 @@
-import pandas as pd
+﻿import pandas as pd
+h = 1
+def safetyDictionary(LiczbaLudnosci):
 
-def safetyDictionary(URL, LiczbaLudnosci):
-    Dane = {}
-    df = pd.read_csv(URL, header=None, encoding="latin1", error_bad_lines=False, sep=";")
-    #print(df)
-    #print(df.iloc[[34]])
+    URL = {'gwałty':"https://www.dane.gov.pl/media/resources/20180521/postepowaniawszczete-zgwalcenie.csv",
+           'morderstwa':"https://www.dane.gov.pl/media/resources/20180521/postepowaniawszczete-zabojstwo.csv",
+           'kradzieże':"https://www.dane.gov.pl/media/resources/20180521/postepowaniawszczete-rozbojnicze.csv"}
 
     Woj = {}
     Woj["KWP Kraków"] = 'Malopolskie'
@@ -25,24 +25,45 @@ def safetyDictionary(URL, LiczbaLudnosci):
     Woj["KWP Poznañ"] = 'Wielkopolskie'
     Woj["KWP £ód"] = 'Lodzkie'
 
-    wojTable = ['Dolnoslaskie', 'Kujawsko-pomorskie', 'Lubelskie', 'Lubuskie', 'Lodzkie', 'Malopolskie', 'Mazowieckie', 'Opolskie', 'Podkarpackie',
-                'Podlaskie', 'Pomorskie', 'Slaskie', 'Swietokrzyskie', 'Warminsko-mazurskie', 'Wielkopolskie', 'Zachodnio-pomorskie' ]
+    wojTable = ['Dolnoslaskie', 'Kujawsko-pomorskie', 'Lubelskie', 'Lubuskie', 'Lodzkie', 'Malopolskie', 'Mazowieckie',
+                'Opolskie', 'Podkarpackie',
+                'Podlaskie', 'Pomorskie', 'Slaskie', 'Swietokrzyskie', 'Warminsko-mazurskie', 'Wielkopolskie',
+                'Zachodnio-pomorskie']
     # nazwa wojewodztwa = wojTable[liczba//2-1]
-    retDictionary = {'Dolnoslaskie':0, 'Kujawsko-pomorskie':0, 'Lubelskie':0, 'Lubuskie':0, 'Lodzkie':0, 'Malopolskie':0, 'Mazowieckie':0, 'Opolskie':0, 'Podkarpackie':0, 'Podlaskie':0, 'Pomorskie':0, 'Slaskie':0, 'Swietokrzyskie':0, 'Warminsko-mazurskie':0, 'Wielkopolskie':0, 'Zachodnio-pomorskie':0}
 
-    tab = ["KWP Kraków", "KWP Rzeszów", "KWP Lublin", "KWP Bia³ystok", "KWP Bydgoszcz", "KWP Gdañsk", "KWP Szczecin", "KWP Wroc³aw", "KWP Opole", "KWP Katowice",
-           "KWP Olsztyn", "KWP Kielce", "KSP Warszawa", "KWP z/s w Radomiu", "KWP Gorzów Wielkpolski", "KWP Poznañ", "KWP £ód"]
+    H = {'morderstwa': 0, 'gwałty': 0, 'kradzieże': 0}
+    F = ['morderstwa', 'gwałty', 'kradzieże']
 
-    for x, y, z in zip(df[df.columns[0]], df[df.columns[1]], df[df.columns[2]]):
-        if x in tab and int(str(y)) > 2015:
-            retDictionary[Woj[x]] += int(str(z))
+    retDictionary = {'Dolnoslaskie': H, 'Kujawsko-pomorskie': H, 'Lubelskie': H, 'Lubuskie': H, 'Lodzkie': H,
+                     'Malopolskie': H, 'Mazowieckie': H, 'Opolskie': H,
+                     'Podkarpackie': H, 'Podlaskie': H, 'Pomorskie': H, 'Slaskie': H, 'Swietokrzyskie': H,
+                     'Warminsko-mazurskie': H,
+                     'Wielkopolskie': H, 'Zachodnio-pomorskie': H, 'Normal' : H}
 
-    maxi = 0
-    for i in retDictionary:
-        if i > maxi:
-            maxi = i
+    tab = ["KWP Kraków", "KWP Rzeszów", "KWP Lublin", "KWP Bia³ystok", "KWP Bydgoszcz", "KWP Gdañsk", "KWP Szczecin",
+           "KWP Wroc³aw", "KWP Opole", "KWP Katowice",
+           "KWP Olsztyn", "KWP Kielce", "KSP Warszawa", "KWP z/s w Radomiu", "KWP Gorzów Wielkpolski", "KWP Poznañ",
+           "KWP £ód"]
 
-    for i in retDictionary:
-        i = i / float(maxi) * 100
+
+
+    for arg in F:
+        df = pd.read_csv(URL[arg], header=None, encoding="latin1", error_bad_lines=False, sep=";")
+        print(arg)
+        for x, y, z in zip(df[df.columns[0]], df[df.columns[1]], df[df.columns[2]]):
+            if x in tab and int(str(y)) > 2015:
+                print(x + " " + y + " " + z)
+                retDictionary[Woj[x]][arg] -= int(z.replace(" ", ""))
+
+        maxi = 0
+        for j in retDictionary:
+            for k in F:
+                if k == arg and retDictionary[j][k] < maxi:
+                    maxi = retDictionary[j][k]
+        retDictionary['Normal'][arg] = maxi
+
+    print(retDictionary)
 
     return retDictionary
+
+safetyDictionary(h)
