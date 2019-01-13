810a23071401e1407336fc192df45189dc75d682
maciektr <maciek2tr99@gmail.com>
2019-01-13 04:27:36 +0100

    Front development

--- api.py
+++ api.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, render_template
 from flask_restful import Resource, Api
 from createDictionary import * 
+import copy
+# from normalizer import normalize
+from functools import reduce
 import evaluate 
 
 data = createDictionary()
@@ -8,15 +11,24 @@ data = createDictionary()
 app = Flask(__name__)
 api = Api(app)
 
-def applyParams(table,params):
-    for key in data:
-        for issue in data[key]:
-            data[key][issue] *= float(params[issue])
+def applyParams(data_,params):
+    table = copy.deepcopy(data_)
+    if reduce((lambda x,y:int(x)*int(y)),list(params.values()))==1:
+        return table
+    for key in table:
+        for issue in table[key]:
+            table[key][issue] *= float(params[issue])
     return table
 
 class regions(Resource):
     def get(self):
         return data
+class regionsApplied(Resource):
+    def get(self,paramSafety,paramHealth,paramEconomy):
+        params = {'Safety':paramSafety,'Health':paramHealth,'Economy':paramEconomy}
+        table = applyParams(data,params)
+        return table
+
 class best(Resource):
     def get(self,paramSafety,paramHealth,paramEconomy):
         params = {'Safety':paramSafety,'Health':paramHealth,'Economy':paramEconomy}
@@ -25,7 +37,7 @@ class best(Resource):
 
 api.add_resource(regions, '/data')
 api.add_resource(best,'/best/<paramSafety>/<paramHealth>/<paramEconomy>')
-
+api.add_resource(regionsApplied,'/table/<paramSafety>/<paramHealth>/<paramEconomy>')
 @app.route('/')
 def index():
    return render_template('index.html')
