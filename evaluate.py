17793c8b76915bc4e913e911de2d9dc883746ee6
maciektr <maciek2tr99@gmail.com>
2019-01-13 05:14:12 +0100

    Getting results

--- evaluate.py
+++ evaluate.py
@@ -1,15 +1,22 @@
 # from random import randint
+import operator
 def best(VarDict):
     # return list(VarDict.keys())[randint(0,len(list(VarDict.keys()))-2)]
-    	max_ = {}
-	res_ = ['', 0]
-	for key in VarDict:
-		for var_ in VarDict[key]:
-			max_[key] += VarDict[key][var_]
-		if max_[key] > res_[1]:
-			res_[1] = max_key
-			res[0] = key
-	
-	return res_	
-
+    '''
+    max_ = {}
+    res_ = ['', 0]
+    for key in VarDict:
+        max_[key]=0
+    for key in VarDict:
+        for var_ in VarDict[key]:
+            max_[key] += VarDict[key][var_]
+        if max_[key] > res_[1]:
+            res_[1] = max_[key]
+            res_[0] = key
+    return res_[0]
+    '''
+    sumDict = {}
+    for key in VarDict:
+        sumDict[key] = sum([VarDict[key][x] for x in ['Economy','Health','Safety']])
+    return max(sumDict.items(), key=operator.itemgetter(1))[0]
 
